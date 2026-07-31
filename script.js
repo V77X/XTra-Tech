99f31178cf96f57bb64995c440c8aa5198cc3101
V77X <xcrasher67@gmail.com>
2026-07-31 18:04:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -271,3 +271,14 @@ document.addEventListener("keydown", e => {
 });
 
 console.log("XTra Tech Ready 🚀");
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registered:", registration.scope);
+      })
+      .catch((error) => {
+        console.log("Service Worker registration failed:", error);
+      });
+  });
+}
